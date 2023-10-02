b839cb91f8c44bcdd77c1382a711ec7b6dc5ad78
WellZup <wellington.oliveira@zup.com.br>
2023-10-02 15:12:57 -0300

    :tada:Inicio do projeto

---
+++ src/test/java/com/squad3/bemestar/BemestarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squad3.bemestar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BemestarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
