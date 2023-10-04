9a2890b05c52a03ec23599dcec727d47ab96ffed
WellZup <wellington.oliveira@zup.com.br>
2023-10-04 16:10:46 -0300

    :zap:Tratamento de exceções na Classe CampanhaController

--- src/main/java/com/squad3/bemestar/controller/CampanhasController.java
+++ src/main/java/com/squad3/bemestar/controller/CampanhasController.java
@@ -19,8 +19,13 @@ public class CampanhasController {
     //Endpoint para listar todas as campanhas
     @GetMapping
     public ResponseEntity<List<Campanhas>> listarCampanhas() {
+        try {
         List<Campanhas> campanhas = campanhasService.listarCampanhas();
         return ResponseEntity.ok(campanhas);
+        } catch (CampanhaNotFoundException e) { return ResponseEntity.notFound().build();}
+        catch (Exception e) {
+            System.out.println(e.getMessage());}
+        return null;
     }
 
     //Endpoint para listar campanhas por id
@@ -38,22 +43,41 @@ public class CampanhasController {
     //Endpoint para criar uma nova campanha
     @PostMapping
     public ResponseEntity<Campanhas> criarCampanha(@RequestBody Campanhas campanhas) {
-        Campanhas campanhasCriada = campanhasService.criarCampanha(campanhas);
-        return ResponseEntity.status(HttpStatus.CREATED).body(campanhasCriada);
+        try {
+            Campanhas campanhasCriada = campanhasService.criarCampanha(campanhas);
+            return ResponseEntity.status(HttpStatus.CREATED).body(campanhasCriada);
+        } catch (Exception e) {
+            System.out.println("Erro ao criar Campanha");
+        }
+        return null;
     }
 
     //Endpoint para atualizar uma campanha
     @PutMapping
     public ResponseEntity<Campanhas> atualizarCampanha(@RequestBody Campanhas campanhas) {
+        try {
+
         Campanhas campanhasAtualizada = campanhasService.atualizarCampanha(campanhas);
         return ResponseEntity.ok(campanhasAtualizada);
+        }catch (CampanhaNotFoundException e ) { return ResponseEntity.notFound().build();}
     }
 
     //Endpoint para deletar uma campanha
     @DeleteMapping("/{id}")
     public void deletarCampanha(@PathVariable Long id) {
-        campanhasService.deletarCampanha(id);
+        try{
+            campanhasService.deletarCampanha(id);
+        ResponseEntity.noContent().build();
+        }catch (CampanhaNotFoundException e) {
+            ResponseEntity.notFound().build();
+        }
+
     }
 
 
+    public class CampanhaNotFoundException extends RuntimeException {
+        public CampanhaNotFoundException(Long id) {
+            super("Campanha com ID " + id + " não encontrada.");
+        }
+    }
 }
