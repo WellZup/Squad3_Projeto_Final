ebabaf8c11ae85e889f368a8ec8d611a2f239bac
WellZup <wellington.oliveira@zup.com.br>
2023-10-11 18:50:22 -0300

    :sparkles:Melhorias

---
+++ src/main/java/com/squad3/bemestar/exception/PerguntaCreationException.java
@@ -0,0 +1,7 @@
+package com.squad3.bemestar.exception;
+
+public class PerguntaCreationException extends RuntimeException{
+    public PerguntaCreationException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/squad3/bemestar/exception/PerguntaNotFoundException.java
@@ -0,0 +1,7 @@
+package com.squad3.bemestar.exception;
+
+public class PerguntaNotFoundException extends RuntimeException{
+    public PerguntaNotFoundException(String message) {
+        super(message);
+    }
+}
