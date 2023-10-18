0f89e9063b4214e85ba9d62e300320365548f14e
carloslaczynskizup <carlos.laczynski@zup.com.br>
2023-10-18 13:56:49 -0300

    Testes Controllers e HTTP Rev.2

--- src/main/java/com/squad3/bemestar/domain/entity/Perguntas.java
+++ src/main/java/com/squad3/bemestar/domain/entity/Perguntas.java
@@ -30,7 +30,7 @@ public class Perguntas {
 
     @NotNull(message = "O campo 'perguntaTexto' não pode ser nulo.")
     @NotBlank(message = "O campo 'perguntaTexto' não pode estar em branco.")
-    @Size(min = 5, max = 100, message = "O campo 'perguntaTexto' deve ter entre 5 e 100 caracteres.")
+    @Size(max = 500, message = "O campo 'perguntaTexto' deve ter no máximo 500 caracteres.")
     @Column(name = "pergunta_texto")
     private String perguntaTexto;
 
